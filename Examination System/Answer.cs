3823587a03192363220a1e5a115af8f092879620
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-07-21 07:48:23 +0300

    Define a class for the answers [ AnswerId, AnswerText ].

---
+++ Examination System/Answer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System
+{
+    internal struct Answer
+    {
+        #region Properties
+
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        #endregion
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        public Answer(int _id, string _text)
+        {
+            Id = _id;
+            Text = _text;
+        }
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        // I Overrided The Equals Method To Compare Between The User's Answer And The Correct Answer
+
+        public override readonly bool Equals([NotNullWhen(true)] object? obj)
+        {
+            return obj is Answer answer && Id == answer.Id;
+
+
+        }
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(Id, Text);
+        }
+
+
+        // --------------------------------------------------------------------------------------------------------------------
+
+
+        public override readonly string ToString()
+        {
+            return $"Answer Id: {Id}\nAnswer Text: {Text}";
+        }
+
+        // --------------------------------------------------------------------------------------------------------------------
+    }
+}
+
