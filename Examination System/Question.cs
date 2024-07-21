eb89d3bfb9aefe69272d55300a722fa6a113bb48
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-07-21 07:44:59 +0300

    Design a Class to represent the Question Object [ Header , Body , Mark ]

---
+++ Examination System/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System
+{
+    internal class Question
+    {
+        #region Properties
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; } 
+        #endregion
+
+    }
+}
