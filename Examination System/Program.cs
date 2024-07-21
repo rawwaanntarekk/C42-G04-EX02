9e11e460411d84bcdbca3f723b48804cc0a8e8a1
Rawan Tarek <115898539+rawwaanntarekk@users.noreply.github.com>
2024-07-21 08:20:31 +0300

    In the Main ,  declare a subject object to create one type of exam.

--- Examination System/Program.cs
+++ Examination System/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Examination_System
+﻿using System.Diagnostics;
+
+namespace Examination_System
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Subject subject = new Subject(1, "C#");
+            subject.CreateExam();
+
+            Console.WriteLine("Do You Want To Start The Exam?");
+            Console.WriteLine("1- Yes\n2- No");
+            int choice;
+            do
+            {
+                Console.Write("Please Enter The Number Of Your Choice: ");
+            } while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 2);
+
+            Console.Clear();
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            if (subject.Exam is not null && choice == 1)
+            {
+                stopwatch.Start();
+                subject.Exam.ShowExam();
+                stopwatch.Stop();
+                Console.WriteLine($"The Exam Time Is: {stopwatch.Elapsed}");
+
+            }
         }
     }
 }
